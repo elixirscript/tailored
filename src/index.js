d89bbfc1f1572d6ef8eb1873f6477b06fe7bff8a
Bryan Joseph <bryanjos@gmail.com>
2016-06-22 14:41:54 -0500

    Add match_or_default to export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import { defmatch, match, MatchError, Clause, clause } from "./tailored/defmatch";
+import { defmatch, match, MatchError, Clause, clause, match_or_default } from "./tailored/defmatch";
 import { variable, wildcard, startsWith, capture, headTail, type, bound, bitStringMatch } from "./tailored/types";
 
 
 export default {
   defmatch, match, MatchError,
   variable, wildcard, startsWith,
-  capture, headTail, type, bound, Clause, clause, bitStringMatch
+  capture, headTail, type, bound,
+  Clause, clause, bitStringMatch,
+  match_or_default
 };
